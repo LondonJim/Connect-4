d54b4e46cc46319377579bb779d9dcd3e734c171
James Sutherland <jrsutherland78@googlemail.com>
2018-11-16 14:48:07 +0000

    feat: two players alternating moves

--- src/game.js
+++ src/game.js
@@ -1,12 +1,13 @@
 class Game{
   constructor(){
     this.board = [['.','.','.','.','.','.'],
-                       ['.','.','.','.','.','.'],
-                       ['.','.','.','.','.','.'],
-                       ['.','.','.','.','.','.'],
-                       ['.','.','.','.','.','.'],
-                       ['.','.','.','.','.','.'],
-                       ['.','.','.','.','.','.']]
+                  ['.','.','.','.','.','.'],
+                  ['.','.','.','.','.','.'],
+                  ['.','.','.','.','.','.'],
+                  ['.','.','.','.','.','.'],
+                  ['.','.','.','.','.','.'],
+                  ['.','.','.','.','.','.']]
+    this.currentPlayer = "0"
   }
 
   playerMove(column){
@@ -15,11 +16,20 @@ class Game{
     var totalRows = this.board.length - 1
     while (rowCount <= totalRows) {
       if (this.board[rowCount][column - 1] === '.'){
-        this.board[rowCount][column - 1] = '0'
+        this.board[rowCount][column - 1] = this.currentPlayer
         this.board = this.board.reverse()
+        this.changePlayer()
         break;
       }
       rowCount ++
+    }
+  }
+
+  changePlayer(){
+    if (this.currentPlayer === "0") {
+      this.currentPlayer = "1"
+    } else {
+      this.currentPlayer = "0"
     }
   }
 }
