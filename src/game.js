f855e597b7ffb121bf5a5003f0df6382de59a566
James Sutherland <jrsutherland78@googlemail.com>
2018-11-16 15:39:24 +0000

    feat: vertical and horizontal checks for win

--- src/game.js
+++ src/game.js
@@ -7,7 +7,8 @@ class Game{
                   ['.','.','.','.','.','.'],
                   ['.','.','.','.','.','.'],
                   ['.','.','.','.','.','.']]
-    this.currentPlayer = "0"
+    this.currentPlayer = "1"
+    this.winner = `Player ${this.currentPlayer} Wins!`
   }
 
   playerMove(column){
@@ -26,10 +27,35 @@ class Game{
   }
 
   changePlayer(){
-    if (this.currentPlayer === "0") {
-      this.currentPlayer = "1"
+    if (this.currentPlayer === "1") {
+      this.currentPlayer = "2"
     } else {
-      this.currentPlayer = "0"
+      this.currentPlayer = "1"
+    }
+  }
+
+  winCheck(){
+    // vertical check
+    for (var x = 0; x < this.board.length - 3; x++) {
+      for (var y = 0; y < this.board[0].length; y++) {
+        if (this.board[x][y] === this.currentPlayer
+            && this.board[x+1][y] === this.currentPlayer
+            && this.board[x+2][y] === this.currentPlayer
+            && this.board[x+3][y] === this.currentPlayer){
+          return this.winner
+        }
+      }
+    }
+    // horizontal check
+    for (var y = 0; y < this.board[0].length; y++) {
+      for (var x = 0; x < this.board.length; x++) {
+        if (this.board[x][y] === this.currentPlayer
+            && this.board[x][y+1] === this.currentPlayer
+            && this.board[x][y+2] === this.currentPlayer
+            && this.board[x][y+3] === this.currentPlayer){
+          return this.winner
+        }
+      }
     }
   }
 }
